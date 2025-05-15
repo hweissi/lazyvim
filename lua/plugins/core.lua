e9d44c5f62ece638b3c2a7513818de6d72600c93
Hannes Weissteiner <hannes.weissteiner@tugraz.at>
2025-05-15 13:49:39 +0200

    asdf

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -11,7 +11,7 @@ return {
     "lunarvim/lunar.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-  }
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -10,6 +10,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     build = function()
+      vim.cmd [[Lazy load markdown-preview.nvim]]
       vim.fn["mkdp#util#install"]()
     end,
   },
