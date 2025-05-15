2e9ea51a40d70f9a994d01fbd3994e6c5f5a8e62
Hannes Weissteiner <hannes.weissteiner@tugraz.at>
2025-05-15 14:43:30 +0200

    asdf

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -12,9 +12,7 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
   },
-  {
-    "catppuccin/nvim"
-  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
   {
     "LazyVim/LazyVim",
     opts = {
