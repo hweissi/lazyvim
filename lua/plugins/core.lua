87d32b6ec6ee3b68bb4b5f271bcfea147b55e112
Hannes Weissteiner <hannes.weissteiner@tugraz.at>
2025-05-15 14:40:20 +0200

    asdf

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -12,7 +12,9 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
   },
-  -- Configure LazyVim to load gruvbox
+  {
+    "catppuccin/nvim"
+  },
   {
     "LazyVim/LazyVim",
     opts = {
